ed6b0799e8927dd4c6acfbd1ca37a772a162e652
CrepeTF <trungnguyen02@outlook.com>
2021-03-10 19:56:18 +0000

    Bump version (again)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         targetSdkVersion(AndroidConfig.targetSdk)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         versionCode = 13
-        versionName = "r342.1"
+        versionName = "r342.2"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
