6a3b433528f417de85f2e0b199ad03bdcee7c5f9
CrepeTF <trungnguyen02@outlookcom>
2021-05-02 17:07:37 +0100

    Cleanup 2

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
@@ -280,13 +280,6 @@ class SettingsGeneralController : SettingsController() {
             titleRes = R.string.pref_category_fork
 
             switchPreference {
-                key = Keys.labelVisibility
-                titleRes = R.string.pref_hide_labels
-                summaryRes = R.string.pref_hide_labels_summary
-                defaultValue = false
-            }
-
-            switchPreference {
                 key = Keys.labels
                 titleRes = R.string.pref_hide_labels
                 summaryRes = R.string.pref_hide_labels_summary
