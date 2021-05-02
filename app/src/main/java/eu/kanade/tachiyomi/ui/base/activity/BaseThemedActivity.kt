Ramsesdb/gexu-legacy-fork

Themes cleanup (#4894)

app/src/main/java/eu/kanade/tachiyomi/ui/base/activity/BaseThemedActivity.kt
@@ -1,65 +1,39 @@
 package eu.kanade.tachiyomi.ui.base.activity
 
-import android.content.res.Configuration
-import android.os.Build
+import android.content.res.Configuration.UI_MODE_NIGHT_MASK
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.preference.PreferenceValues.DarkThemeVariant
+import eu.kanade.tachiyomi.data.preference.PreferenceValues.LightThemeVariant
+import eu.kanade.tachiyomi.data.preference.PreferenceValues.ThemeMode
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import uy.kohesive.injekt.injectLazy
-import eu.kanade.tachiyomi.data.preference.PreferenceValues as Values
 
 abstract class BaseThemedActivity : AppCompatActivity() {
 
     val preferences: PreferencesHelper by injectLazy()
 
-    private val isDarkMode: Boolean by lazy {
-        val themeMode = preferences.themeMode().get()
-        (themeMode == Values.ThemeMode.dark) ||
-            (
-                themeMode == Values.ThemeMode.system &&
-                    (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)
-                )
-    }
-
-    private val lightTheme: Int by lazy {
-        when (preferences.themeLight().get()) {
-            Values.LightThemeVariant.blue -> R.style.Theme_Tachiyomi_LightBlue
-            else -> {
-                when {
-                    // Light status + navigation bar
-                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1 -> {
-                        R.style.Theme_Tachiyomi_Light_Api27
-                    }
-                    // Light status bar + fallback gray navigation bar
-                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
-                        R.style.Theme_Tachiyomi_Light_Api23
-                    }
-                    // Fallback gray status + navigation bar
-                    else -> {
-                        R.style.Theme_Tachiyomi_Light
-                    }
-                }
-            }
-        }
-    }
-
-    private val darkTheme: Int by lazy {
-        when (preferences.themeDark().get()) {
-            Values.DarkThemeVariant.blue -> R.style.Theme_Tachiyomi_DarkBlue
-            Values.DarkThemeVariant.amoled -> R.style.Theme_Tachiyomi_Amoled
-            else -> R.style.Theme_Tachiyomi_Dark
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(
-            when {
-                isDarkMode -> darkTheme
-                else -> lightTheme
+        val isDarkMode = when (preferences.themeMode().get()) {
+            ThemeMode.light -> false
+            ThemeMode.dark -> true
+            ThemeMode.system -> resources.configuration.uiMode and UI_MODE_NIGHT_MASK == UI_MODE_NIGHT_YES
+        }
+        val themeId = if (isDarkMode) {
+            when (preferences.themeDark().get()) {
+                DarkThemeVariant.default -> R.style.Theme_Tachiyomi_Dark
+                DarkThemeVariant.blue -> R.style.Theme_Tachiyomi_Dark_Blue
+                DarkThemeVariant.amoled -> R.style.Theme_Tachiyomi_Dark_Amoled
             }
-        )
-
+        } else {
+            when (preferences.themeLight().get()) {
+                LightThemeVariant.default -> R.style.Theme_Tachiyomi_Light
+                LightThemeVariant.blue -> R.style.Theme_Tachiyomi_Light_Blue
+            }
+        }
+        setTheme(themeId)
         super.onCreate(savedInstanceState)
     }
 }

CrepeTF/Takoyomi

(cherry picked from commit ad57fde1c55984f3235ce86a7505e3675039b1dc)
# Conflicts:
#	app/src/main/java/eu/kanade/tachiyomi/ui/base/activity/BaseThemedActivity.kt
#	app/src/main/res/values/themes.xml
(cherry picked from commit e43777bba767442e97e121ea2682d7c2bba14285)

app/src/main/java/eu/kanade/tachiyomi/ui/base/activity/BaseThemedActivity.kt
@@ -1,65 +1,41 @@
 package eu.kanade.tachiyomi.ui.base.activity
 
-import android.content.res.Configuration
-import android.os.Build
+import android.content.res.Configuration.UI_MODE_NIGHT_MASK
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.preference.PreferenceValues.DarkThemeVariant
+import eu.kanade.tachiyomi.data.preference.PreferenceValues.LightThemeVariant
+import eu.kanade.tachiyomi.data.preference.PreferenceValues.ThemeMode
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import uy.kohesive.injekt.injectLazy
-import eu.kanade.tachiyomi.data.preference.PreferenceValues as Values
 
 abstract class BaseThemedActivity : AppCompatActivity() {
 
     val preferences: PreferencesHelper by injectLazy()
 
-    private val isDarkMode: Boolean by lazy {
-        val themeMode = preferences.themeMode().get()
-        (themeMode == Values.ThemeMode.dark) ||
-            (
-                themeMode == Values.ThemeMode.system &&
-                    (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)
-                )
-    }
-
-    private val lightTheme: Int by lazy {
-        when (preferences.themeLight().get()) {
-            Values.LightThemeVariant.default -> when {
-                // Light status + navigation bar
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1 -> {
-                    R.style.Theme_Tachiyomi_Light_Api27
-                }
-                // Light status bar + fallback gray navigation bar
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
-                    R.style.Theme_Tachiyomi_Light_Api23
-                }
-                // Fallback gray status + navigation bar
-                else -> {
-                    R.style.Theme_Tachiyomi_Light
-                }
-            }
-        }
-    }
-
-    private val darkTheme: Int by lazy {
-        when (preferences.themeDark().get()) {
-            Values.DarkThemeVariant.amoled -> R.style.Theme_Tachiyomi_Amoled
-            Values.DarkThemeVariant.red -> R.style.Theme_Tachiyomi_Red
-            Values.DarkThemeVariant.midnightdusk -> R.style.Theme_Tachiyomi_MidnightDusk
-            Values.DarkThemeVariant.lime -> R.style.Theme_Tachiyomi_Lime
-            Values.DarkThemeVariant.hotpink -> R.style.Theme_Tachiyomi_HotPink
-            else -> R.style.Theme_Tachiyomi_Dark
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(
-            when {
-                isDarkMode -> darkTheme
-                else -> lightTheme
+        val isDarkMode = when (preferences.themeMode().get()) {
+            ThemeMode.light -> false
+            ThemeMode.dark -> true
+            ThemeMode.system -> resources.configuration.uiMode and UI_MODE_NIGHT_MASK == UI_MODE_NIGHT_YES
+        }
+        val themeId = if (isDarkMode) {
+            when (preferences.themeDark().get()) {
+                DarkThemeVariant.default -> R.style.Theme_Tachiyomi_Dark
+                DarkThemeVariant.amoled -> R.style.Theme_Tachiyomi_Dark_Amoled
+                DarkThemeVariant.red -> R.style.Theme_Tachiyomi_Dark_Red
+                DarkThemeVariant.midnightdusk -> R.style.Theme_Tachiyomi_Dark_MidnightDusk
+                DarkThemeVariant.lime -> R.style.Theme_Tachiyomi_Lime
+                DarkThemeVariant.hotpink -> R.style.Theme_Tachiyomi_Dark_HotPink
             }
-        )
-
+        } else {
+            when (preferences.themeLight().get()) {
+                LightThemeVariant.default -> R.style.Theme_Tachiyomi_Light
+            }
+        }
+        setTheme(themeId)
         super.onCreate(savedInstanceState)
     }
 }