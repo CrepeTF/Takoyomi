8fc27236ace8da2a3dc532a5f4f7bbc3e5b7a503
CrepeTF <trungnguyen02@outlookcom>
2022-04-16 12:27:15 +0100

    Append 3.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "eu.kanade.tachiyomi.sy.crepe"
         minSdk = AndroidConfig.minSdk
         targetSdk = AndroidConfig.targetSdk
-        versionCode = 45
-        versionName = "3.2.0"
+        versionCode = 46
+        versionName = "3.2.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
